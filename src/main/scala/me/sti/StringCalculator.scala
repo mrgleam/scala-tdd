c16c5a785092780f82d3eb5c36b5a0b9bf66abf8
Santi <santi@odds.team>
2019-10-09 23:04:56 +0700

    refactor

--- src/main/scala/me/sti/StringCalculator.scala
+++ src/main/scala/me/sti/StringCalculator.scala
@@ -1,12 +1,13 @@
 package me.sti
 
 class StringCalculator(rawDigits: String) {
-  val negative_numbers = numbers filter { _ < 0 }
   require(negative_numbers isEmpty)
 
   def add = (0 /: numbers) { _ + _ } // numbers.foldLeft(0)(_+_)
 
   def numbers = digits split ",|\n" map { _.toInt }
+
+  def negative_numbers = numbers filter { _ < 0 }
 
   def digits = {
     val delimiter_extractor = "//(.)\n(.*)".r
