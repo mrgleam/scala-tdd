8ee3a98c0f342eba5722312fcdf747262ac7f596
Santi <santi@odds.team>
2019-10-09 22:27:58 +0700

    add next fail test

--- src/test/scala/me/sti/StringCalculatorSpec.scala
+++ src/test/scala/me/sti/StringCalculatorSpec.scala
@@ -7,5 +7,8 @@ class StringCalculatorSpec extends WordSpec with Matchers {
     "return 0 for empty string" in {
       new StringCalculator("").add shouldBe 0
     }
+    "return 1 for 1" in {
+      new StringCalculator("1").add shouldBe 1
+    }
   }
 }
