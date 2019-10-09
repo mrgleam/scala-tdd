3d63c502b0cc2a24ff430e2d469abe221b2a0aa3
Santi <santi@odds.team>
2019-10-09 22:38:40 +0700

    refactor

--- src/main/scala/me/sti/StringCalculator.scala
+++ src/main/scala/me/sti/StringCalculator.scala
@@ -3,11 +3,11 @@ package me.sti
 class StringCalculator(digits: String) {
   def add = {
     if (digits.length == 0) 0 else {
-      val numbers = digits split ","
+      val numbers = digits split "," map { _.toInt }
       if (numbers.length == 1)
-        numbers(0).toInt
+        numbers(0)
       else
-        numbers(0).toInt + numbers(1).toInt
+        numbers(0) + numbers(1)
     }
   }
 }
