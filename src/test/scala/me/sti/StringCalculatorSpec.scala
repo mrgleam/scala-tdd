c3d5f1273b67a4cce87915043229a298280a1003
Santi <santi@odds.team>
2019-10-09 22:24:54 +0700

    add first fail test

---
+++ src/test/scala/me/sti/StringCalculatorSpec.scala
@@ -0,0 +1,11 @@
+package me.sti
+
+import org.scalatest.{ Matchers, WordSpec }
+
+class StringCalculatorSpec extends WordSpec with Matchers {
+  "StringCalculatorSpec" should {
+    "return 0 for empty string" in {
+      new StringCalculator("").add shouldBe 0
+    }
+  }
+}
