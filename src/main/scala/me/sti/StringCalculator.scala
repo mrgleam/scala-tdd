d3ac93fbf17c5a0999cc3e7dd9e2a7933f3bc0eb
Santi <santi@odds.team>
2019-10-09 22:36:33 +0700

    pass the red

--- src/main/scala/me/sti/StringCalculator.scala
+++ src/main/scala/me/sti/StringCalculator.scala
@@ -2,6 +2,12 @@ package me.sti
 
 class StringCalculator(digits: String) {
   def add = {
-    if (digits.length == 0) 0 else digits.toInt
+    if (digits.length == 0) 0 else {
+      val numbers = digits split ","
+      if (numbers.length == 1)
+        digits.toInt
+      else
+        numbers(0).toInt + numbers(1).toInt
+    }
   }
 }
