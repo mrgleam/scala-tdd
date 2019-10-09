a1752351cb66d3063bb95f55d5c0b5398e250c22
Santi <santi@odds.team>
2019-10-09 22:53:07 +0700

    pass the red

--- src/main/scala/me/sti/StringCalculator.scala
+++ src/main/scala/me/sti/StringCalculator.scala
@@ -1,6 +1,6 @@
 package me.sti
 
-class StringCalculator(digits: String) {
+class StringCalculator(rawDigits: String) {
   def add = {
     if (digits.length == 0) 0 else {
       (0 /: numbers) { _ + _ } // numbers.foldLeft(0)(_+_)
@@ -8,4 +8,13 @@ class StringCalculator(digits: String) {
   }
 
   def numbers = digits split ",|\n" map { _.toInt }
+
+  def digits = {
+    val delimiter_extractor = "//(.)\n(.*)".r
+
+    rawDigits match {
+      case delimiter_extractor(delimiter, digits) => digits.replace(delimiter, ",")
+      case _ => rawDigits
+    }
+  }
 }
