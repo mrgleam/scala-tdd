edabc5e4e2d4221af596e3f9fa5ec911a2a6c3bc
Santi <santi@odds.team>
2019-10-09 23:03:14 +0700

    pass the red

--- src/main/scala/me/sti/StringCalculator.scala
+++ src/main/scala/me/sti/StringCalculator.scala
@@ -1,6 +1,9 @@
 package me.sti
 
 class StringCalculator(rawDigits: String) {
+  val negative_numbers = numbers filter { _ < 0 }
+  require(negative_numbers isEmpty)
+
   def add = (0 /: numbers) { _ + _ } // numbers.foldLeft(0)(_+_)
 
   def numbers = digits split ",|\n" map { _.toInt }
